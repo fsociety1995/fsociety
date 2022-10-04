de1af91e486008a54ed6f59b88da4df65fa40c22
fsociety1995 <shaquillegrif@gmail.com>
2022-10-04 09:31:24 +0200

    Update HelloWorld.php

--- HelloWorld.php
+++ HelloWorld.php
@@ -5,4 +5,15 @@ echo "Hello World \n";
 echo "Hello World \n";
 echo "Hello World \n";
 
+echo "einde opdracht 1"
+
+$tekst="Hello World \n"
+
+echo $tekst;
+echo $tekst;
+echo $tekst;
+echo $tekst;
+echo $tekst;
+
+
 ?>
