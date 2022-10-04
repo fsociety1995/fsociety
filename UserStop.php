a6c09c4dd66cc265a3875eeb1268363cde038b8b
fsociety1995 <shaquillegrif@gmail.com>
2022-10-04 10:54:21 +0200

    Create UserStop.php

---
+++ UserStop.php
@@ -0,0 +1,14 @@
+<?php
+
+$stop = "nee";
+$teller = 1;
+
+while ( $stop == "nee" ) {
+    
+    echo "Dit is run $teller\n";
+
+    $stop = readline ("Wil je stoppen [ja/nee] : ");
+    $teller++;
+}
+
+?>
